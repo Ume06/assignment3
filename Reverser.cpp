7662bbaf43819a2e0dc589247158019d344b952e
Jack Duggan <jacksdggn@gmail.com>
2025-03-22 15:08:39 +1030

    Add reverseDigit

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,9 +1,20 @@
 #include "Reverser.h"
 
-int Reverser::reverseDigit(int input) {
+#include <cmath>
+#include <iostream>
 
+int Reverser::reverseDigit(int input) {
+  if (input < 0) return -1;      // invalid input check
+  if (input < 10) return input;  // non-reversible int
+
+  int lastDigit = input % 10;
+  int next = input / 10;
+  int power = log10(input);
+  
+  return lastDigit * pow(10, power) + reverseDigit(next);
 }
 
 std::string Reverser::reverseString(std::string input) {
-  
+  if (input.length() < 1) return "ERROR";
+  return "a";
 }
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Reverser.h"
+int main() {
+  Reverser x;
+  std::cout << x.reverseDigit(54321) << std::endl;
+}
